12a6c55d0ba825bac9ab57306e9ab0ef363a333a
Edward Mara <e.james.mara@gmail.com>
2019-06-07 21:48:44 -0700

    working question and answer display and choice resolution

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TriviaGame
+
+Initially had seperate arrays for questions, answers and choices. Ran into trouble when attempting to iterate through them and their corresponding answers. Rather than spend longer trying to make that work, switched each question to be its own obj that has choices and an answer index.
+
+Made a function resolveChoice() to accept a userinput argument and determine whether or not it was correct or not.  Having this resolve inside the newgame function made the user input static after the second choice, and would loop without choosing a new answer for every question.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,37 @@
+// $(document).ready(function () {
 var numberCorrect = 0;
 var numberIncorrect = 0;
-var remainingQuestions = 10;
-var currentQuestion = 1;
+//start on question 1 (index 0) in qAndA array
+var currentQuestion = 0;
+var questionAnswered;
 
-
-var qAndA = {
-
-    questions: {
-        one: "Which Star Trek captain has an artificial heart?",
-        two: "What character owns a bar on Deep Space Nine?",
-        three: "What is Data's brother's name?",
-        four: "Which Star Trek captain loves baseball?",
+var qAndA = [
+    {
+        question: "Which Star Trek captain has an artificial heart?",
+        choice: ["James T. Kirk", "Jean-Luc Picard", "Kathryn Janeway", "Benjamin Sisko"],
+        answer: 1
     },
-
-    correctAnswer: {
-        one: "Jean-Luc Picard",
-        two: "Quark",
-        three: "Lore",
-        four: "Benjamin Sisko",
+    {
+        question: "What character owns a bar on Deep Space Nine?",
+        choice: ["Odo", "Worf", "Quark", "Data"],
+        answer: 2
     },
-
-    allAnswers: {
-        one: ["James T. Kirk", "Jean-Luc Picard", "Kathryn Janeway", "Christopher Pike"],
-        two: ["Odo", "Worf", "Quark", "Data"],
-        three: ["Lore", "Borg", "Doran", "Phillip"],
-        four: ["James T. Kirk", "Jean-Luc Picard", "Kathryn Janeway", "Christopher Pike"],
-
+    {
+        question: "What is Data's brother's name?",
+        choice: ["Lore", "Lore", "Borg", "Doran", "Phillip"],
+        answer: 0
     },
-}
-
+    {
+        question: "Which Star Trek captain loves baseball?",
+        choice: ["James T. Kirk", "Jean-Luc Picard", "Kathryn Janeway", "Benjamin Sisko"],
+        answer: 3
+    }
+]
 
 function newGame() {
     numberCorrect = 0;
     numberIncorrect = 0;
-    remainingQuestions = 10;
+    // remainingQuestions = 10;
     currentQuestion = 1;
 
     //div containing game
@@ -43,6 +41,57 @@ function newGame() {
     $("#stats").text("");
 
     //remove intro
-    $("#start-button").hide();
+    // $("#start-button").hide();
+
+
+
+
 
 }
+
+function resolveChoice(userInput, y) {
+    var questionDisplayed = qAndA[y];
+    if (userInput === questionDisplayed.choice[questionDisplayed.answer]) {
+        console.log("correct answer");
+        numberCorrect++;
+        currentQuestion++;
+        return true;
+    } else {
+        console.log("incorrect answer");
+        numberIncorrect--;
+        currentQuestion++;
+        return false;
+    }
+
+}
+
+function acceptInput() {
+
+
+}
+
+function newQuestion(x) {
+    console.log(x);
+    var questionDisplayed = qAndA[x];
+    //display question and answer choices from specified qandA set
+    $("#question").text(questionDisplayed.question);
+    $("#answer1").text(questionDisplayed.choice[0]);;
+    $("#answer2").text(questionDisplayed.choice[1]);;
+    $("#answer3").text(questionDisplayed.choice[2]);;
+    $("#answer4").text(questionDisplayed.choice[3]);;
+
+
+}
+
+$(".aButton").on("click", function () {
+    var userAnswer = $(this).text();
+    console.log(userAnswer);
+    resolveChoice(userAnswer, currentQuestion);
+    newQuestion(currentQuestion);
+});
+
+
+newQuestion(currentQuestion);
+
+
+

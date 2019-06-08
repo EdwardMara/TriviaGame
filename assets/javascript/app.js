14b540697245e9a30d6c687eefe6e16855aa1f33
Edward Mara <e.james.mara@gmail.com>
2019-06-07 18:47:03 -0700

    initial page commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+var numberCorrect = 0;
+var numberIncorrect = 0;
+var remainingQuestions = 10;
+var currentQuestion = 1;
+
+
+var qAndA = {
+
+    questions: {
+        one: "Which Star Trek captain has an artificial heart?",
+        two: "What character owns a bar on Deep Space Nine?",
+        three: "What is Data's brother's name?",
+        four: "Which Star Trek captain loves baseball?",
+    },
+
+    correctAnswer: {
+        one: "Jean-Luc Picard",
+        two: "Quark",
+        three: "Lore",
+        four: "Benjamin Sisko",
+    },
+
+    allAnswers: {
+        one: ["James T. Kirk", "Jean-Luc Picard", "Kathryn Janeway", "Christopher Pike"],
+        two: ["Odo", "Worf", "Quark", "Data"],
+        three: ["Lore", "Borg", "Doran", "Phillip"],
+        four: ["James T. Kirk", "Jean-Luc Picard", "Kathryn Janeway", "Christopher Pike"],
+        
+    },
+}
+function newGame(){
+
+}
